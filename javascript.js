7d5362f6f24cbffca74c543d979884fe8d75a231
steve omari <steveomari62@gmail.com>
2022-11-16 21:15:48 -0500

    create container and connect

--- javascript.js
+++ javascript.js
@@ -1,1 +1,7 @@
-console.log('Hello World!');
+console.log('Hello World');
+
+//16x16 grid of square divs in js
+
+//connect to data attribute
+const masterContainer = document.querySelector(`[data-container='master-container']`);
+masterContainer.textContent=('Hello again!');
