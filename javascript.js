6f3ef4ef5071e28f38529f8ef5a42d41e0b947ca
steve omari <steveomari62@gmail.com>
2022-11-17 21:58:46 -0500

    create grid

--- javascript.js
+++ javascript.js
@@ -5,3 +5,22 @@ console.log('Hello World');
 //connect to data attribute
 const masterContainer = document.querySelector(`[data-container='master-container']`);
 masterContainer.textContent=('Hello again!');
+
+// const table = document.querySelector(`[data-container='table']`);
+// const tr= document.createElement('tr');
+// const td= document.createElement('td');
+
+for (let i = 1; i < 17; i++) {
+    const table = document.querySelector(`[data-container='table']`);
+    const tr= document.createElement('tr');
+    table.appendChild(tr);   
+
+    for (let j = 1; j < 17; j++) {
+        const td= document.createElement('td');
+        tr.appendChild(td);            
+    }
+}
+
+
+
+
